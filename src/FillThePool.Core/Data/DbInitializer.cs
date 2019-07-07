8dbc0659b8e32eb94364e1c2c488b5668586f121
watsoncolin <colin@colinwatson.me>
2019-07-07 13:23:33 -0600

    add auto db migration

---
+++ src/FillThePool.Core/Data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FillThePool.Core.Data
+{
+	public static class DbInitializer
+	{
+		public static void Initialize(ApplicationDbContext context)
+		{
+			context.Database.Migrate();
+		}
+	}
+}
--- src/FillThePool.Core/Program.cs
+++ src/FillThePool.Core/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using FillThePool.Core.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace FillThePool.Core
@@ -14,7 +16,29 @@ namespace FillThePool.Core
 	{
 		public static void Main(string[] args)
 		{
-			CreateWebHostBuilder(args).Build().Run();
+			var host = CreateWebHostBuilder(args).Build();
+
+			using (var scope = host.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+				try
+				{
+					var environment = services.GetRequiredService<IHostingEnvironment>();
+
+					if (!environment.IsDevelopment())
+					{
+						var context = services.GetRequiredService<ApplicationDbContext>();
+						DbInitializer.Initialize(context);
+					}
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An error occurred while seeding the database.");
+				}
+			}
+
+			host.Run();
 		}
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
