b8876a03d33c787966b478a0861778fc5264af01
watsoncolin <colin@colinwatson.me>
2019-07-07 13:40:03 -0600

    fix bug

--- src/FillThePool.Core/Data/DbInitializer.cs
+++ src/FillThePool.Core/Data/DbInitializer.cs
@@ -17,6 +17,7 @@ namespace FillThePool.Core.Data
 			{
 				waitlistTemplate = new EmailTemplate
 				{
+					Type = "Waitlist",
 					Subject = "Stansbury Swim Lessons Available",
 					Html = "You can now purchase more credits at Stansbury Swim. This invitation is valid for 7 days.  You'll be able to make one transaction then you'll need to add yourself back to the wait list if you need more.  <a href='https://www.stansburyswim.com/pricing'>Click here to purchase more credits.</a>",
 					CreatedOn = DateTime.UtcNow,
