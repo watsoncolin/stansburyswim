97652ef529194efafa546491d77143fe28b1cbca
watsoncolin <colin@colinwatson.me>
2020-04-28 20:03:14 -0600

    adds pools page and about image

---
+++ src/FillThePool.Core/Pages/Pools.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FillThePool.Core.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FillThePool.Core.Pages
+{
+    public class PoolsModel : PageModel
+	{
+		private readonly ApplicationDbContext _context;
+		public PoolsModel(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public IList<Pool> Pools { get; set; }
+		public void OnGet()
+        {
+			Pools = _context.Pools.Where((p) => p.Active).ToList();
+        }
+    }
+}
