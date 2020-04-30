6664710584800259141d7d1315dc12a2cedfb3e7
watsoncolin <colin@colinwatson.me>
2020-04-29 20:01:50 -0600

    adds purchase summary

--- src/FillThePool.Core/Areas/Admin/Pages/Manage/Index.cshtml.cs
+++ src/FillThePool.Core/Areas/Admin/Pages/Manage/Index.cshtml.cs
@@ -1,11 +1,28 @@
+using FillThePool.Core.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
 
 namespace FillThePool.Core.Areas.Admin.Pages
 {
 	public class IndexModel : PageModel
-    {
+	{
+		private readonly ApplicationDbContext _context;
+		public IndexModel(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+
+		public int LessonCreditCount { get; set; }
+		public int ScheduledCount{ get; set; }
 		public void OnGet()
 		{
+			if(User.Claims.Any(c => c.Type == "Admin" && c.Value == "Full"))
+			{
+				LessonCreditCount = _context.Transactions.Where(t => t.LessonCredit > 0).Sum((t) => t.LessonCredit);
+				ScheduledCount = _context.Transactions.Where(t => t.LessonCredit < 0).Sum((t) => Math.Abs(t.LessonCredit));
+			}
 		}
 	}
 }
