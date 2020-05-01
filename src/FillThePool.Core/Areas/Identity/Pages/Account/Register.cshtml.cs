96883ded4d87f3bcb2023dc99de895f369580e25
watsoncolin <colin@colinwatson.me>
2020-05-01 08:38:57 -0600

    add full url to email confirmation.

--- src/FillThePool.Core/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/FillThePool.Core/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,10 +80,9 @@ namespace FillThePool.Core.Areas.Identity.Pages.Account
                         protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a> or navigate to {HtmlEncoder.Default.Encode(callbackUrl)}");
 
 					// Force new users to confirm email address.
-					// await _signInManager.SignInAsync(user, isPersistent: false);
 					return RedirectToPage("PleaseConfirmEmail");
                 }
                 foreach (var error in result.Errors)
